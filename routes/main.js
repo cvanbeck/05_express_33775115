b330b74dd7808073f22f019f77681307c7ac14c6
Charlie <charlievanbeck@gmail.com>
2025-10-28 10:59:12 +0000

    Added route handlers to main.js

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,12 @@
 const express = require("express")
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+router.get("/about", (req, res) => {
+    res.render("about.ejs")
+})
+
 module.exports = router;

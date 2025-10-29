920d36414d842fd72fbf24dc496f4f4be12f626b
Charlie <charlievanbeck@gmail.com>
2025-10-29 12:01:57 +0000

    Added escape characters for tab instead of actual tabs to make log screen fit in ide screen

--- utils/logger.js
+++ utils/logger.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 
 const logger = (req, res, next) => {
     date = new Date().toLocaleString("en-gb")
-    fs.appendFile("./logs/requests.txt", `${date} \n    IP: ${req.ip} \n    Endpoint: ${req.originalUrl}\n \n`, err => {
+    fs.appendFile("./logs/requests.txt", `${date} \n\tIP: ${req.ip} \n\tEndpoint: ${req.originalUrl}\n \n`, err => {
         if(err){
             console.log("File failed to save")
         } else {
